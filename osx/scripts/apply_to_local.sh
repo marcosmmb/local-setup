89add3684f24a207f67129d4b0247031694ef86e
Marcos Monteiro <marcos@Marcoss-Mini.localdomain>
2025-03-31 18:17:06 -0700

    Add ssh config file

--- osx/scripts/apply_to_local.sh
+++ osx/scripts/apply_to_local.sh
@@ -1,9 +1,14 @@
 # apply files from repo into local directories
+echo "Applying files from repository to local"
+
 echo "Copying .zprofile"
 cp ./osx/home/.zprofile ~/.zprofile
 
 echo "Copying .zshrc"
 cp ./osx/home/.zshrc ~/.zshrc
+
+echo "Copying .ssh"
+cp ./osx/home/ssh ~/.ssh/config
 
 echo "Copying skhd"
 cp -r ./osx/home/config/skhd/skhdrc ~/.skhdrc
--- osx/scripts/replicate_from_local.sh
+++ osx/scripts/replicate_from_local.sh
@@ -1,9 +1,14 @@
 # replicate local files into repo
+echo "Replicating file from local to repository"
+
 echo "Copying .zprofile"
 cp ~/.zprofile ./osx/home/
 
 echo "Copying .zshrc"
 cp ~/.zshrc ./osx/home/
+
+echo "Copying .ssh"
+cp ~/.ssh/config ./osx/home/ssh
 
 echo "Copying skhd"
 cp -r ~/.skhdrc ./osx/home/config/skhd/skhdrc
