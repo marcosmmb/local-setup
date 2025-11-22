2b64a9500f63ac98c2a0b1e1bdde18669f20a8a8
Marcos Monteiro <marcosmmb90@gmail.com>
2025-11-21 23:06:11 -0800

    Simply osx/install.sh by adding a function

--- osx/install.sh
+++ osx/install.sh
@@ -1,90 +1,49 @@
-echo "Installing tools"
+#!/usr/bin/env bash
 
+install() {
+    local package_name="$1"
+    local cask="$2"
+
+    if [[ -z "$package_name" ]]; then
+        echo "Error: package_name is required."
+        return 1
+    fi
+
+    if [[ "$cask" == "cask" ]]; then
+        echo "Installing cask: $package_name..."
+        brew install --cask "$package_name"
+    else
+        echo "Installing package: $package_name..."
+        brew install "$package_name"
+    fi
+}
+
+echo "Installing tools"
 # install homebrew
 echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# install docker\
-echo "Installing Docker"
-brew install --cask docker
-docker --version
 
-# install stats
-echo "Installing Stats"
-brew install --cask stats
-
-# install displaylink manager
-echo "Installing DisplayLink Manager"
-brew install --cask displaylink
-
-# install yabai
-# https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)
-echo "Installing yabai"
-brew install koekeishiya/formulae/yabai
+install docker cask
+install stats cask
+install displaylink cask
+koekeishiya/formulae/yabai
 yabai --start-service
-
-# install skhd
-# https://github.com/koekeishiya/skhd
-echo "Installing skhd"
-brew install koekeishiya/formulae/skhd
+install koekeishiya/formulae/skhd
 skhd --start-service
-
-# install btop
-echo "Installing btop"
-brew install btop
-
-# install github cli
-echo "Installing Github CLI"
-brew install gh
-
-# install tabby
-echo "Installing Tabby"
-brew install --cask tabby
-
-# install vs code
-echo "Installing VS Code"
-brew install --cask visual-studio-code
-
-# install brave browser
-echo "Installing Brave"
-brew install --cask brave-browser
-
-# install bitwarden
-echo "Installing Bitwarden"
-brew install --cask bitwarden
-
-# install moonlight
-echo "Installing Moonlight"
-brew install --cask moonlight
-
-# install raycast
-echo "Installing Raycast"
-brew install raycast
-
-# install ffmpeg
-echo "Installing ffmpeg"
-brew install ffmpeg
-
-# install nmap
-echo "Installing nmap"
-brew install nmap
-
-# install ipython
-echo "Installing iPython"
-brew install ipython
-
-# install pipx
-echo "Installing pipx"
-brew install pipx
-
-# install aws-cli
-echo "Installing aws-cli"
-brew install awscli
-
-# install obsidian
-echo "Installing obsidian"
-brew install --cask obsidian
-
-# install Spotify
-echo "Installing Spotify"
-brew install --cask spotify
+install btop
+install gh
+install tabby cask
+install visual-studio-code cask
+install brave-browser cask
+install bitwarden cask
+install moonlight cask
+install raycast
+install ffmpeg
+install nmap
+install ipython
+install pipx
+install awscli
+install obsidian cask
+install spotify cask
+install gnupg
