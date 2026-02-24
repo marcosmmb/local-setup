9e5ddf49bec1c20df5df333be6cfdfd2245710af
Marcos Monteiro <marcosmmb90@gmail.com>
2026-02-24 10:38:03 -0800

    backup files before overriding

--- osx/apply_to_local.sh
+++ osx/apply_to_local.sh
@@ -2,16 +2,21 @@
 echo "Applying files from repository to local"
 
 echo "Copying .zprofile"
+cp ~/.zprofile ~/.zprofile.old
 cp ./osx/home/.zprofile ~/.zprofile
 
 echo "Copying .zshrc"
+cp ~/.zshrc ~/.zshrc.old
 cp ./osx/home/.zshrc ~/.zshrc
 
 echo "Copying .ssh"
+cp ~/.ssh/config ~/.ssh/config.old
 cp ./osx/home/ssh ~/.ssh/config
 
 echo "Copying skhd"
+cp -r ~/.skhdrc ~/.skhdrc_old
 cp -r ./osx/home/config/skhd/skhdrc ~/.skhdrc
 
 echo "Copying yabai"
+cp -r ~/.yabairc ~/.yabairc_old
 cp -r ./osx/home/config/yabai/yabairc ~/.yabairc
